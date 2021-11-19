c3bdb34f703370beee918501e532c19d5bb0d4b6
nurizati <91452441+nurizati@users.noreply.github.com>
2021-11-19 16:56:38 +0800

    jadual

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-function addNumbers() {
+function darab() {
     var number;
-    var result = "";
+    var result = "<table>";
 
     number = Number(document.getElementById("number").value);
 
-    for(var i = 1; i<= 11; i++){
-      result = result + "<p>"+number + "*" + i + "=" + number * i+"</p>";
+    for (var i = 1; i <= 11; i++) {
+        result = result + "<tr><td>" + number + "</td><td>X</td><td>" + i + "</td><td>=</td><td>" + number * i + "</td></tr>";
     }
-
+    result += "</table>";
     document.getElementById("result").innerHTML = result;
-  }
+}
+
+
+
